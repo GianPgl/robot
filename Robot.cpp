a046dd8386405a728730461df47198977ca07856
Andrea Santoro <dedo24397@gmail.com>
2017-08-18 22:19:06 +0200

    Need some adjustment when returning NO_ECHO

--- Robot.cpp
+++ Robot.cpp
@@ -15,13 +15,14 @@ void Robot::init(){
     analogWrite(pwmLPin, speed);
     analogWrite(pwmRPin, speed);
     sonar.init(trigPin, echoPin);
+    //lcd.init(clkPin, dinPin, dcPin, resetPin);
     brake(true);
 }
 
 /******************* MOVEMENT *******************/
 uint8_t Robot::controlSpeed(uint8_t speed) {
   if(speed > maxSpeed)
-  return maxSpeed;
+    return maxSpeed;
   return speed;
 }
 
@@ -45,9 +46,6 @@ void Robot::goForward() {
   brake(false);
   digitalWrite(directionRPin, HIGH);
   digitalWrite(directionLPin, HIGH);
-  /*Serial.begin(9600);
-  Serial.println("I'm going forward!");
-  Serial.end();*/
 }
 
 void Robot::goBackward() {
@@ -56,25 +54,20 @@ void Robot::goBackward() {
   digitalWrite(directionLPin, LOW);
 }
 
-void Robot::turnRight() {
-  brake(false);
-  digitalWrite(directionRPin, HIGH);
-  digitalWrite(brakeLPin, LOW);
-  /*Serial.begin(9600);
-  Serial.println("I'm going right!");
-  Serial.end();*/
-}
-
 void Robot::turnLeft() {
   brake(false);
-  digitalWrite(directionLPin, HIGH);
-  digitalWrite(brakeRPin, LOW);
-  /*Serial.begin(9600);
-  Serial.println("I'm going left!");
-  Serial.end();*/
+  digitalWrite(directionRPin, HIGH);
+  digitalWrite(directionLPin, LOW);
 }
 
-void Robot::rotateOn(uint8_t direction, uint16_t ms) {
+void Robot::turnRight() {
+  brake(false);
+  digitalWrite(directionLPin, HIGH);
+  digitalWrite(directionRPin, LOW);
+}
+
+void Robot::rotateOn(uint8_t direction, uint16_t deg) {
+  brake(false);
   if (direction == LEFT) {
     digitalWrite(directionRPin, LOW);
     digitalWrite(directionLPin, HIGH);
@@ -82,19 +75,28 @@ void Robot::rotateOn(uint8_t direction, uint16_t ms) {
     digitalWrite(directionRPin, HIGH);
     digitalWrite(directionLPin, LOW);
   }
-  delay(ms);
+  delay(map(deg, 0, 360, 0, 4*getDelay()));
+  brake(true);
 }
 
 void Robot::rightAngleRotation(uint8_t direction) {
-  rotateOn(direction, DELAY); /* Tempo provvisorio */
+  rotateOn(direction, 90);
 }
 
-void Robot::halfRotation() {
-  rotateOn(RIGHT, 2*DELAY); /* Tempo provvisorio (2* tempo right) */
+void Robot::rotate(uint8_t direction, uint16_t deg){
+  uint16_t _deg = deg > 360 ? 360 : deg;  //needs more than a full rotation if deg > 360, so it does a stepper one
+  rotateOn(direction, _deg);
+  if(deg > 360){
+    rotateOn(direction, deg - 360);
+  }
+}
+
+void Robot::halfRotation(uint8_t direction) {
+  rotateOn(direction, 180);
 }
 
 void Robot::fullRotation() {
-  rotateOn(RIGHT, 4*DELAY); /* Tempo provvisorio (2 * tempo half)*/
+  rotateOn(RIGHT, 360);
 }
 
 /******************* SERVO *******************/
@@ -104,7 +106,7 @@ void Robot::setServo(bool state){
     if(servo.attached())
       return;
     servo.attach(servoPin);
-    servo.write(90);
+    servo.write(90-20);
   } else {
     if(!servo.attached())
       return;
@@ -114,7 +116,7 @@ void Robot::setServo(bool state){
 
 void Robot::servoRotation(uint8_t degrees) { /* set servo position */
   if(!servo.attached())
-    this->setServo(true);
+    setServo(true);
   servo.write(degrees);
   delay(100); /* wait 100 milliseconds to permit to the servo to reach the position */
 }
@@ -130,35 +132,49 @@ uint8_t Robot::readDistanceIN() {
   return distance;
 }
 
-bool Robot::isClear() { /* return if the path is clear or not */
-    if(readDistanceCM() <= safeDistance)
-        return false;
-    return true;
+bool Robot::isClear() { /* checks path clearness */
+  uint8_t distance = readDistanceCM();
+    if(distance >= safeDistance)
+        return true;
+    return false;
 }
 
-/******************* OBLASTACLE AVOIDING *******************/
+/******************* OBLASTACLE AVOIDANCE *******************/
+
+void Robot::updateDir(uint8_t newDir){
+    prevDir[0] = prevDir[1];
+    prevDir[1] = currDir[0];
+    currDir[0] = currDir[1];
+    currDir[1] = newDir;
+    if(prevDir[0] != -1)
+      emptyDir = false;
+}
+
+inline void Robot::resetDir(){
+  prevDir[0]=prevDir[1]=currDir[0]=currDir[1]=-1;
+  emptyDir = true;
+}
+
 uint8_t Robot::findPath() {
   uint8_t leftDistance = 0;
   uint8_t frontDistance = 0;
   uint8_t rightDistance = 0;
 
   /* Imposto la posizione base del servo */
-  uint8_t position = 45;
+  uint8_t position = 45-20;
   if(!servo.attached())
-    this->setServo(true);
+    setServo(true);
   /* Misuro la distanza variando progressivamente la posizione del servo */
   do {
     servoRotation(position);
     switch (position) {
-      case 45: leftDistance = readDistanceCM(); break;
-      case 90: frontDistance = readDistanceCM(); break;
-      case 135: rightDistance = readDistanceCM(); break;
+      case 45-20: leftDistance = readDistanceCM(); break;
+      case 90-20: frontDistance = readDistanceCM(); break;
+      case 135-20: rightDistance = readDistanceCM(); break;
     }
     position += 45;
     delay(400);
-  } while (position <= 135);
-
-  /* Ritorno la strada migliore */
+  } while (position <= 135-20);
   if (rightDistance > frontDistance) {
     if (rightDistance > leftDistance)
       return rightDistance < safeDistance ? NO_WAY : RIGHT;
@@ -167,28 +183,55 @@ uint8_t Robot::findPath() {
   } else {
     if(leftDistance > frontDistance)
       return leftDistance < safeDistance ? NO_WAY : LEFT;
+    else
+      return frontDistance < safeDistance ? NO_WAY : FRONT;
   }
-  return frontDistance < safeDistance ? NO_WAY : FRONT;
+}
+
+void Robot::findSafeZone(){
+  uint8_t right, left;  //get rotation direction or go back if there's no way to ratate
+  servoRotation(0);
+  delay(400);
+  right = readDistanceCM();
+  servoRotation(180-20);
+  delay(400);
+  left = readDistanceCM();
+  if(right < ROTATION_DIST && left < ROTATION_DIST){
+    servoRotation(90-20);
+    goBackward();
+    while(readDistanceCM() < safeDistance -1)
+      delay(10); //last position from which it can't go forward and get stuck again
+    brake(true);
+  }
+  else{
+    servoRotation(90-20);
+    halfRotation(right >= left ? RIGHT : LEFT); //both-NO_ECHO case means RIGHT
+  }
+  resetDir();
 }
 
 void Robot::setPath() {
   uint8_t direction = findPath();
-  if(direction == NO_WAY){
-    halfRotation();
-    findPath();
+  while(direction == NO_WAY){
+    findSafeZone();
+    direction = findPath();
   }
-  else{
-    switch(direction){
-      case FRONT:
-        goForward();
-      break;
-      case RIGHT:
-        turnRight();
-      break;
-      case LEFT:
-        turnLeft();
-      break;
+  if(direction != FRONT){
+    updateDir(direction);
+    if(stuck()){
+      findSafeZone();
+      direction = findPath();
     }
+    rotate(direction == RIGHT ? RIGHT : LEFT ,45);
+  }
+  servoRotation(90-20);
+  delay(400);
+  if(isClear()){
+    goForward();
+    while(isClear())
+      delay(10);
+    brake(true);
+    resetDir();
   }
 }
 
@@ -212,3 +255,10 @@ void Robot::followLine(){
   }
   //Serial.end();
 }
+
+/**************************LCD*********************/
+
+/*void Robot::setScreen(){
+  lcd.begin();
+  //lcd.display();
+}*/
--- Robot.h
+++ Robot.h
@@ -4,53 +4,63 @@
 
 #include "NewPing.h"
 #include "Servo.h"
+//#include "Adafruit_PCD8544.h"
+//#include "Adafruit_GFX.h"
 
 /* Direction constants */
 #define FRONT 0
 #define RIGHT 1
 #define LEFT 2
-#define NO_WAY 3
-#define DELAY 50
+#define NO_WAY -1
+#define ROTATION_DIST 8
+//#define DELAY 1000
 
 class Robot {
 public:
-    /*Robot(): speed(50), sonar(trigPin, echoPin, 200), obstacle(false) {
-      pinMode(midLineFollower,INPUT);
-      pinMode(rightLineFollower,INPUT);
-      pinMode(leftLineFollower,INPUT);
-    };*/
-    Robot(uint8_t _speed = 50): speed(_speed)/*, sonar(trigPin, echoPin, 200), obstacle(false)*/ {
-      /*pinMode(midLineFollower,INPUT);
-      pinMode(rightLineFollower,INPUT);
-      pinMode(leftLineFollower,INPUT);*/
-    };
+    //(90 / speed) * 1000 is the time to make a 90 degrees rotation
+    //_speed/2 is due to brake time variation
+    Robot(uint8_t _speed = 100): speed(_speed), _delay(90000/_speed - _speed/2), safeDistance(_speed/10+_speed/20){}
     void init();
     void setSpeed(uint8_t);
+    uint16_t getDelay() {return _delay;}
     void brake(bool stop);
     void goForward();
     void goBackward();
     void turnRight();
     void turnLeft();
+    void rotate(uint8_t direction, uint16_t deg);
     void rightAngleRotation(uint8_t);
     void fullRotation();
-    void halfRotation();
+    void halfRotation(uint8_t);
 
     void servoRotation(uint8_t);
 
     uint8_t readDistanceCM();
     uint8_t readDistanceIN();
     bool isClear();
-
-    uint8_t findPath();
+    bool stuck(){return !emptyDir && cmpPrevDir();}
+    void findSafeZone();
     void setPath();
-    //void changePath();
 
     void followLine();
 
     void setServo(bool);
 
+    //compare current directions with previous ones
+    bool cmpPrevDir(){return currDir[0]==prevDir[0] && currDir[1]==prevDir[1];}
+
+    bool emptyDir = true;
+
+    //void setScreen();
+
 private:
-    int speed;
+    uint8_t speed;
+    uint16_t _delay;
+    uint8_t safeDistance;
+    uint8_t prevDir[2]; //previous directions
+    uint8_t currDir[2]; //current directions
+    void updateDir(uint8_t);
+    void resetDir();
     static const uint8_t maxSpeed = 255;
     static const uint8_t halfSpeed = 127;
     uint8_t controlSpeed(uint8_t);
@@ -71,14 +81,22 @@ private:
     static const uint8_t servoPin = 10;
 
     Servo servo; /* Servo object to control the servo motor */
+    uint8_t findPath();
 
-    uint8_t trigPin = A1; /* It can be changed */
-    uint8_t echoPin = A2; /* It can be changed */
+    static const uint8_t trigPin = A1; /* It can be changed */
+    static const uint8_t echoPin = A2; /* It can be changed */
     NewPing sonar; /* NewPing object to control the ultrasonic sensor, sets with trig and echo pins and maximum distance*/
 
-    static const uint8_t dangerDistance = 20;
-    static const uint8_t safeDistance = 20;
-    bool obstacle;
+    /* LCD pins*/
+    static const uint8_t clkPin = 4;
+    static const uint8_t dinPin = 2;
+    static const uint8_t dcPin = 0;
+    static const uint8_t resetPin = 1;
+
+    //Adafruit_PCD8544 lcd;/*Adafruit_PCD8544 object to manage lcd screen*/
+
+    //static const uint8_t dangerDistance = 10;
+    bool obstacleDetected;
 };
 
 
